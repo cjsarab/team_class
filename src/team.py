7f45509e5d00ad1b5e79afe524531b4712d11cfc
Cammy Smith <cjsarab1991@gmail.com>
2022-09-05 15:51:04 +0100

    Team Class

---
+++ src/team.py
@@ -0,0 +1,17 @@
+class Team:
+    def __init__(self, name, players, coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
+    
+    def has_player(self, player_name):
+        for player in self.players:
+            if player == player_name:
+                return True
+        
+        for player in self.players:
+            if player != player_name:
+                return False
